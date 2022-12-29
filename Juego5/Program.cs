e07e349ca9fa93e4a0780484186b1f6144b59716
Shiki <jigen99@hotmail.com>
2022-12-29 19:57:18 -0300

    comentari master 5

--- Juego5/Program.cs
+++ Juego5/Program.cs
@@ -29,6 +29,7 @@ namespace Juego5
             //comentario 7 rama1
             // comentario 3 master
             //Comentario master 4
+            //comentario master 5
             Console.SetWindowSize(62, 20);
             Console.SetBufferSize(62, 20);
             Console.CursorVisible = false;
